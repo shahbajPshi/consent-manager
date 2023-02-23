8e08f14ac866cbd6211a0e88eb13fcaba45be662
shahbajlive <105340930+shahbajlive@users.noreply.github.com>
2023-02-23 20:44:00 +0530

    [Setup][wip] Adding integration and unit tests

---
+++ src/test/java/com/pshi/consentmanager/link/ClientRegistryTest.java
@@ -0,0 +1,71 @@
+package com.pshi.consentmanager.link;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pshi.consentmanager.link.discovery.model.Provider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static reactor.core.publisher.Mono.when;
+
+public class ClientRegistryTest {
+//    any ClientRequest instance passed as an argument to a method during
+//    the test can be captured and inspected using the captor variable.
+    @Captor
+    ArgumentCaptor<ClientRequest> captor;
+
+    @Mock
+    private ExchangeFunction exchangeFunction;
+
+    ClientRegistry clientRegistry;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+        WebClient.Builder webClientBuilder = WebClient.builder()
+                .exchangeFunction(exchangeFunction);
+        ClientRegistryProperties clientRegistryProperties = new ClientRegistryProperties();
+        clientRegistry = new ClientRegistry(webClientBuilder, clientRegistryProperties);
+    }
+
+    @Test
+    void getProvidersByGivenName() throws IOException {
+        var source = new ObjectMapper().readValue(
+                ClassLoader.getSystemClassLoader().getResource("provider.json"),
+                new TypeReference<List<Provider>>() {}
+        );
+        var jsonNode = new ObjectMapper().readValue(
+                ClassLoader.getSystemClassLoader().getResource("provider.json"),
+                new TypeReference<List<JsonNode>>() {}
+        );
+        when(exchangeFunction.exchange(captor.capture())).thenReturn(
+                Mono.just(ClientResponse.create(HttpStatus.OK)
+                        .header("Content-Type", "application/json")
+                        .body(jsonNode.toString()).build()
+        ));
+
+        StepVerifier.create(clientRegistry.providersOf("Max"))
+                .assertNext(provider -> {
+                    assertThat(provider.getName()).isEqualTo(source.get(0).getName());
+                })
+                .verifyComplete();
+
+        assertThat(captor.getValue().url().toString()).isEqualTo("null/providers?name=Max");
+    }
+}
---
+++ src/test/java/com/pshi/consentmanager/link/discovery/DiscoveryTest.java
@@ -0,0 +1,48 @@
+package com.pshi.consentmanager.link.discovery;
+
+import com.pshi.consentmanager.link.ClientRegistry;
+import com.pshi.consentmanager.link.discovery.model.Address;
+import com.pshi.consentmanager.link.discovery.model.Telecom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+import static com.pshi.consentmanager.link.discovery.TestBuilders.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DiscoveryTest {
+
+    private Discovery discovery;
+
+    @Mock
+    ClientRegistry clientRegistry;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void providersOfCalledWithMax() {
+        discovery = new Discovery(clientRegistry);
+        Address address = address().use("work").build();
+        Telecom telecom = telecom().use("work").build();
+        var provider = provider()
+                .addresses(List.of(address))
+                .telecoms(List.of(telecom))
+                .name("max")
+                .build();
+        when(clientRegistry.providersOf("Max")).thenReturn(Flux.just(provider));
+
+        discovery.providersFrom("Max");
+
+        verify(clientRegistry).providersOf("Max");
+        assertThat(clientRegistry.providersOf("Max").collectList().block().get(0)).isEqualTo(provider);
+    }
+}
